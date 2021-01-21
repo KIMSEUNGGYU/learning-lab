7b0cd2f0e5d636d7230a8d929263aee0c5baa20b
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-01-21 18:24:50 +0900

    update project structure

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-
 const app = express();
+const port = process.env.PORT || "3000";
+
+const controller = require("./controller");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
+app.use("/", controller);
+
+app.listen(port);
+// app.listen(port, () => {
+//   console.log(`Application is listening at port: ${port}`);
+// });
 
 module.exports = app;
